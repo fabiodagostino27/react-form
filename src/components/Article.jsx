7d823ca779f5e503ef6c249fda5b46dc0ec2cb86
fabiodagostino27 <fabiodago27@gmail.com>
2025-02-24 16:26:20 +0100

    ADD: edit atricle function

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -6,11 +6,20 @@ export const Article = (props) => {
         setArticlesArray(updatedArray);
     };
 
+    const editArticle = (i) => {
+        let articleToEdit = articlesArray[i];
+        const newTitle = prompt("Modifica il titolo dell'articolo.");
+        articleToEdit = newTitle;
+        const updatedArray = [...articlesArray];
+        updatedArray[i] = newTitle;
+        setArticlesArray(updatedArray)
+    }
+
     return (
         <li className="list-group-item d-flex justify-content-between align-items-center">
             {props.article}
             <div>
-                <button className="btn btn-outline-secondary me-1"><i className="fa-solid fa-pen"></i></button>
+                <button className="btn btn-outline-secondary me-1" onClick={() => editArticle(props.articleIndex)}><i className="fa-solid fa-pen"></i></button>
                 <button className="btn btn-outline-danger" onClick={() => removeArticle(props.articleIndex)}><i className="fa-solid fa-trash"></i></button>
             </div>
         </li>
