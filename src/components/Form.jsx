fbdbd31efa7e7696ad232c1c0633b90f1e5fb855
fabiodagostino27 <fabiodago27@gmail.com>
2025-02-24 15:39:07 +0100

    ADD: adding new articles through form

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,9 +1,17 @@
-export const Form = () => {
+export const Form = (props) => {
+    const {newArticle, setNewArticle, articlesArray, setArticlesArray} = props;
+    
+    const handleSubmit = () => {
+        event.preventDefault();
+        setArticlesArray([...articlesArray, newArticle]);
+        console.log(articlesArray)
+    }
+
     return (
         <div>
             <h4>Aggiungi un articolo</h4>
-            <form className="d-flex">
-                <input className="form-control w-50 me-2" type="text" placeholder="Srivi qui il titolo dell'articolo"/>
+            <form className="d-flex" onSubmit={handleSubmit}>
+                <input className="form-control w-50 me-2" type="text" placeholder="Srivi qui il titolo dell'articolo" value={newArticle} onChange={(e) => setNewArticle(e.target.value)}/>
                 <button className="btn btn-primary">Invio</button>
             </form>
         </div>
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,18 +1,20 @@
+import { useState } from "react";
 import { Article } from "./Article";
 import { Form } from "./Form";
 
 export const Main = (props) => {
     const articles = props.articles;
 
+    const [articlesArray, setArticlesArray] = useState(articles);
+    const [newArticle, setNewArticle] = useState("");
+
     return (
         <main className="container">
             <h3>Ecco gli ultimi articoli:</h3>
             <ul className="list-group list-group-flush mb-4"> 
-                {
-                    articles.map(element => <Article article={element}/>)
-                }
+                {articlesArray.map((element, i) => <Article key={i} article={element}/>)}
             </ul>
-            <Form/>
+            <Form newArticle={newArticle} setNewArticle={setNewArticle} articlesArray={articlesArray} setArticlesArray={setArticlesArray}/>
         </main>
     )
 }
